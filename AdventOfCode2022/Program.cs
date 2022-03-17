6a2889bc6998e8587d2dc3af1775bc1919658bf1
Stefano Prosperi <step.prosperi@gmail.com>
2022-03-17 17:45:55 +0100

    Moved class Day2 to file Day2.cs

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 
 namespace AdventOfCode2022
 {
@@ -12,68 +11,6 @@ namespace AdventOfCode2022
 
             Console.WriteLine(Day2.Part1(input));
             Console.WriteLine(Day2.Part2(input));
-        }
-    }
-
-    class Day2
-    {
-        public static int Part1(string input)
-        {
-            var values = input.Split(Environment.NewLine).Select(l => l.Split(' ')).ToArray();
-
-            int d = 0;
-            int p = 0;
-
-            foreach (string[] s in values)
-            {
-                switch (s[0])
-                {
-                case "forward":
-                    p += int.Parse(s[1]);
-                    break;
-
-                case "down":
-                    d += int.Parse(s[1]);
-                    break;
-
-                case "up":
-                    d -= int.Parse(s[1]);
-                    break;
-                }
-            }
-
-            return d * p;
-        }
-
-        public static int Part2(string input)
-        {
-            var values = input.Split(Environment.NewLine).Select(l => l.Split(' ')).ToArray();
-
-            int d = 0;
-            int p = 0;
-            int a = 0;
-
-            foreach (string[] s in values)
-            {
-                int x = int.Parse(s[1]);
-                switch (s[0])
-                {
-                case "forward":
-                    p += x;
-                    d += a * x;
-                    break;
-
-                case "down":
-                    a += x;
-                    break;
-
-                case "up":
-                    a -= x;
-                    break;
-                }
-            }
-
-            return d * p;
         }
     }
 }
