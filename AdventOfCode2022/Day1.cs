09984669a1259dfc1fe9aa715ed9b3d868835a2b
Stefano Prosperi <step.prosperi@gmail.com>
2022-03-17 17:44:57 +0100

    Add project files.

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2022
+{
+    class Day1
+    {
+        public static int Part1(string input)
+        {
+            var values = input.Split(Environment.NewLine).Select(int.Parse).ToArray();
+
+            return values.Zip(values[1..]).Count(pair => pair.Second > pair.First);
+        }
+
+        public static int Part2(string input)
+        {
+            var values = input.Split(Environment.NewLine).Select(int.Parse).ToArray();
+            int sum    = values[..3].Sum();
+            int i      = 1;
+
+            int result = 0;
+
+            while (i + 3 <= values.Length)
+            {
+                Console.WriteLine(sum);
+
+                int nextSum = values[i..(i + 3)].Sum();
+                result += nextSum > sum ? 1 : 0;
+
+                sum = nextSum;
+                i++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2022
+{
+    class Program
+    {
+        private static void Main()
+        {
+            string input = File.ReadAllText("inputs/input.txt");
+
+            Console.WriteLine(Day2.Part1(input));
+            Console.WriteLine(Day2.Part2(input));
+        }
+    }
+
+    class Day2
+    {
+        public static int Part1(string input)
+        {
+            var values = input.Split(Environment.NewLine).Select(l => l.Split(' ')).ToArray();
+
+            int d = 0;
+            int p = 0;
+
+            foreach (string[] s in values)
+            {
+                switch (s[0])
+                {
+                case "forward":
+                    p += int.Parse(s[1]);
+                    break;
+
+                case "down":
+                    d += int.Parse(s[1]);
+                    break;
+
+                case "up":
+                    d -= int.Parse(s[1]);
+                    break;
+                }
+            }
+
+            return d * p;
+        }
+
+        public static int Part2(string input)
+        {
+            var values = input.Split(Environment.NewLine).Select(l => l.Split(' ')).ToArray();
+
+            int d = 0;
+            int p = 0;
+            int a = 0;
+
+            foreach (string[] s in values)
+            {
+                int x = int.Parse(s[1]);
+                switch (s[0])
+                {
+                case "forward":
+                    p += x;
+                    d += a * x;
+                    break;
+
+                case "down":
+                    a += x;
+                    break;
+
+                case "up":
+                    a -= x;
+                    break;
+                }
+            }
+
+            return d * p;
+        }
+    }
+}
